2a7b561d4045cd39f28bfefa13708a819f7fc16b
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2020-12-23 12:43:23 -0800

    Updated constructors for the stock object

--- Stock.java
+++ Stock.java
@@ -4,11 +4,18 @@
 public class Stock
 {
 
+    // Initializing the polygon API
+    PolygonAPI polygonAPI = new PolygonAPI();
+
+    // Initializing the alpaca API
+    AlpacaAPI alpacaAPI = new AlpacaAPI();
+
     // Variables
     String ticker; // Stock name
     float price; // Current price
+    float close; // Previous day's close
+    float open; // Open of the day
     float beta; // Current beta
-    float weekPrice; // Stock price 1 week ago
     int stake; // Number of stocks we have currently
 
     // Default constructor
@@ -18,9 +25,21 @@ public class Stock
 
         ticker = "";
         price = 0.0;
+        close = 0.0;
         beta = 0.0;
-        weekPrice = 0.0;
         stake = 0;
+
+    }
+
+    // Normal constructor
+    public Stock (String name)
+    {
+
+        ticker = name;
+        price = polygonAPI.getLastTrade(ticker);
+        close = polygonAPI.getPreviousClose(ticker,true); // Second value is making results not being adjusted for splits
+        open = alpacaAPI.getOpenPositionBySymbol(ticker);
+        stake = alpacaAPI.getAssetBySymbol(ticker);
 
     }
 }
